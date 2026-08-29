14d44ccb2b52b654098be9635e70396eea6c1a73
Nalin-Kumar2004 <nalinnow@gmail.com>
2026-08-29 14:43:24 +0530

    feat: initialize project dependencies and configure ESLint for React environment

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -16,5 +16,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'react/prop-types': 'off',
+    'react/no-unknown-property': 'off',
+    'no-unused-vars': 'warn'
   },
 }
